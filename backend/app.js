89c39cac5adcedef80a5238ad69690a92c420d6d
Maksym Dmitriiev <95755128+MaxDM2021@users.noreply.github.com>
2023-04-20 13:46:44 +0300

    21

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 // app.use(express.json());
 
-app.use('/api/countries', express.static('public').json());
+app.use('/api/countries', express.static('public'));
 
 app.use('/api/countries', countriesRouter);
 
